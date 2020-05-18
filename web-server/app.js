578bea6faab1c0fcecee7a1838559935d44cd4af
Sergio Tabares <alu0101124896@ull.edu.es>
2020-05-18 21:12:45 +0000

    Portfolio completed

--- web-server/app.js
+++ web-server/app.js
@@ -8,7 +8,7 @@ APP.set('port', 8080);
 //tell express that we want to use the www folder
 //for our static assets
 APP.use(EXPRESS.static(PATH.join(__dirname, '../www')));
-APP.use(EXPRESS.static(PATH.join(__dirname, '..')));
+APP.use(EXPRESS.static(PATH.join(__dirname, '../..')));
 
 // Listen for requests
 const SERVER = APP.listen(APP.get('port'), '0.0.0.0', function () {
